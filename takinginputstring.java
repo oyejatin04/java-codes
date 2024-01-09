7dbb328c877658b5fc64cd4fecdc6d2f3f8c3f9b
oyejatin04 <113293529+oyejatin04@users.noreply.github.com>
2024-01-10 01:14:04 +0530

    Add files via upload

---
+++ takinginputstring.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class takinginputstring {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name .");
+        String num1 = sc .next();
+        System.out.println("  jatin loves you soo much <3: " + num1 );
+
+    }
+}
