9c3332d7ab198623f8e2af18b1f4df432900b72e
oyejatin04 <113293529+oyejatin04@users.noreply.github.com>
2024-01-10 23:34:14 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,10 @@
+public class arithmetic {
+    public static void main(String[] args) {
+       int  p = 20 , q = 19;
+        System.out.println(p+q);
+        System.out.println(p-q);
+        System.out.println(p*q);
+        System.out.println(p/q);
+        System.out.println(p%q);
+    }
+}
